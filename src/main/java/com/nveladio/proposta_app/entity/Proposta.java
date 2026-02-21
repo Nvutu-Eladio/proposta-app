398abf59af28c73c3e3a1912c4284a6e9fa97718
Nvutu-Eladio <nveladio@gmail.com>
2026-02-20 23:50:40 -0300

    feat: mapeando entidades

---
+++ src/main/java/com/nveladio/proposta_app/entity/Proposta.java
@@ -0,0 +1,26 @@
+package com.nveladio.proposta_app.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovada;
+
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+}
---
+++ src/main/java/com/nveladio/proposta_app/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.nveladio.proposta_app.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String sobreNome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private String renda;
+
+    @OneToOne(mappedBy = "usuario")
+    private Proposta proposta;
+
+
+}
