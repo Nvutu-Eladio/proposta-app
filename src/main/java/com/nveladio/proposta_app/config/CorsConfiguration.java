ef4555341a21007c752695bf76f2a18bffed011a
Nvutu-Eladio <nveladio@gmail.com>
2026-03-18 21:36:19 -0300

    feat: ajustando cors

--- src/main/java/com/nveladio/proposta_app/config/CorsConfiguration.java
+++ src/main/java/com/nveladio/proposta_app/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost/")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("*");
     }
 
